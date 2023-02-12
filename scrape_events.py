be3a26a3edae9d132d74049865f0e473d631685f
petebrown <15838790+petebrown@users.noreply.github.com>
2023-02-12 18:57:05 -0500

    Rewrote to use functions

--- scrape_events.py
+++ scrape_events.py
@@ -1,71 +1,83 @@
 from bs4 import BeautifulSoup
-import datetime as dt
 import pandas as pd
 import requests
 import concurrent.futures
-
-MAX_THREADS = 30
+import re
 
 def get_game_list():
     df = pd.read_csv("https://raw.githubusercontent.com/petebrown/update-player-stats/main/data/players_df.csv")
-
     df.sb_game_id = df.sb_game_id.str.replace("tpg", "")
-
     df["url"] = df.apply(lambda x: f"https://www.soccerbase.com/matches/additional_information.sd?id_game={x.sb_game_id}", axis=1)
 
     games = df[["url", "venue"]].drop_duplicates().to_dict("records")
 
     return games
 
-def make_record(game_id, venue, player_id, event):
+def create_record(game_id, player_id, min_off, min_so, min_on):
     record = {
         "game_id": game_id,
-        "venue": venue,
         "player_id": player_id,
-        "event_details": event.text.strip(),
+        "min_on": min_on,
+        "min_off": min_off,
+        "min_so": min_so
         }
     return record
 
-def get_player_id(sub_text):
-    player_id = sub_text.find("a")["href"].split("=")[1]
+def get_player_id(event_text):
+    player_id = event_text.find("a")["href"].split("=")[1]
     return player_id
 
-def get_events(doc, venue, game_id, side):
+def scrape_starter_events(event):
+    regex = r"\((\d+)\)|\(s\/o (\d+)\)"
+    matches = re.search(regex, event)
+    min_off = matches.group(1)
+    min_so = matches.group(2)
+    return [min_off, min_so]
+
+def scrape_sub_events(event):
+    regex = r"\((\d+)-?(\d+)?(?:, s\/o )?(\d+)?\)"
+    matches = re.search(regex, event)
+    if matches:
+        min_on = matches.group(1)
+        min_off = matches.group(2)
+        min_so = matches.group(3)
+        return [min_on, min_off, min_so]
+
+def get_events(doc, game_id, side):
     match_events = []
 
     subs_off = doc.select(f'.lineup .{side} .firstTeam .replaced')
-    subs_on = doc.select(f'.lineup .{side} .reserve tr:not(.replaced)')
-    dub_subs = doc.select(f'.lineup .{side} .reserve tr')
-    red_cards = doc.select(f'.lineup .{side} .sendingOff')
+    red_cards = doc.select(f'.lineup .{side} .firstTeam .sendingOff')
 
-    for sub in subs_off:
-        player_id = get_player_id(sub)
-        record = make_record(game_id, venue, player_id, sub)
-        match_events.append(record)
+    starter_events = subs_off + red_cards
+    sub_events = doc.select(f'.lineup .{side} .reserve tr')
 
-    for sub in subs_on:
-        player_id = get_player_id(sub)
-        record = make_record(game_id, venue, player_id, sub)
-        match_events.append(record)
-
-    for sub in dub_subs:
-        if '-' in sub.text:
-            player_id = get_player_id(sub)
-
-            record_1 = make_record(game_id, venue, player_id, sub)
-            record_2 = make_record(game_id, venue, player_id, sub)
-
-            match_events.append(record_1)
-            match_events.append(record_2)
-        else:
-            next
-
-    for red in red_cards:
-        player_id = get_player_id(red)
-
-        record = make_record(game_id, venue, player_id, red)
+    for event in starter_events:
+        player_id = get_player_id(event)
+        events = scrape_starter_events(event.text)
+        
+        min_off = events[0]
+        min_so = events[1]
+        
+        record = create_record(game_id, player_id, min_off, min_so, min_on = None)
         
         match_events.append(record)
+
+    for event in sub_events:
+        regex = r"\((\d+)-?(\d+)?(?:, s\/o )?(\d+)?\)"
+        matches = re.search(regex, event.text)
+        if matches:
+            player_id = get_player_id(event)
+            events = scrape_sub_events(event.text)
+        
+            min_on = events[0]
+            min_off = events[1]
+            min_so = events[2]
+            
+            record = create_record(game_id, player_id, min_off, min_so, min_on)
+        
+            match_events.append(record)
+
     return match_events
 
 def scrape_events(game_dict):
@@ -83,10 +95,12 @@ def scrape_events(game_dict):
     r = requests.get(url)
     doc = BeautifulSoup(r.text, 'html.parser')
 
-    events = get_events(doc, venue, game_id, side)
+    events = get_events(doc, game_id, side)
     return events
 
 def async_scraping(scrape_function, urls):
+    MAX_THREADS = 30
+
     threads = min(MAX_THREADS, len(urls))
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
@@ -94,20 +108,20 @@ def async_scraping(scrape_function, urls):
 
     return results
 
-def get_events_df():
+def clean_events_list(input_list):
+    events = list(input_list)
+    events = [event for sublist in events for event in sublist]
+    return events
+
+def main():
     games = get_game_list()
 
-    df = async_scraping(scrape_events, games)
-    df = list(df)
-    df = [sub for sublist in df for sub in sublist]
-    df = pd.DataFrame(df)
+    events = async_scraping(scrape_events, games)
+    events = clean_events_list(events)
     
-    df = df[(df.event_details.str.contains("\(\d+-\d+\)", regex=True)) | (df.event_details.str.contains("\(\d+", regex=True)) | (df.event_details.str.contains("s/o"))].copy()
-    df["min_on"] = df.event_details.str.extract(r"\((\d+)")
-    df["min_off"] = df.event_details.str.extract(r"\-(\d+)\)")
-    df["min_so"] = df.event_details.str.extract(r"s/o (\d+)\)")
-    df = df[["game_id", "player_id", "min_on", "min_off", "min_so", "event_details"]].drop_duplicates().reset_index(drop=True)
+    df = pd.DataFrame(events)
+    
     return df
 
-df = get_events_df()
+df = main()
 df.to_csv("./data/subs-and-reds.csv", index=False)
